1f5cf4a76667f2b57ea48ef216043eab8b40ee39
limingfeng <limingfeng@didiglobal.com>
2019-09-12 22:48:58 +0800

    更新

--- init_database.py
+++ init_database.py
@@ -16,106 +16,14 @@ def init_db(cursor):
 	except Exception as err:
 		print('err:',err)
 
-def clear_table(cursor):
+def clear_data(cursor):
 	print('clear_table')
 	table_list=('test','user_group','user','account','category','category_sub','record')
 	for item in table_list:
 		try:
-			cursor.execute('DROP TABLE '+item)
+			cursor.execute('delete from '+item)
 		except Exception as err:
 			print('err:',err)
-
-
-def create_table(cursor):
-
-	create_test_sql="""
-		CREATE TABLE test (
-  		_id int(10) unsigned NOT NULL AUTO_INCREMENT,
-  		id int(10) unsigned unique NOT NULL,
-  		name text ,
-  		PRIMARY KEY (_id)
-		) COMMENT = '测试表';
-	"""
-
-	create_user_group_sql="""
-		CREATE TABLE user_group (
-  		_id int(10) unsigned NOT NULL AUTO_INCREMENT,
-  		id int(10) unsigned unique  NOT NULL,
-  		name varchar(200) NOT NULL comment '用户组名称',
-    	avatar  text comment '头像路径',
-  		auth int(2) unsigned default 0 comment '权限字段',
-    	PRIMARY KEY (_id)
-		) COMMENT = '用户组表';
-	"""
-
-	create_user_sql="""
-		CREATE TABLE user (
-  		_id int(10) unsigned NOT NULL AUTO_INCREMENT,
-  		id int(10) unsigned unique  NOT NULL,
-  		group_id int(10) unsigned  NOT NULL,
-  		account varchar(100)   NOT NULL,
-  		pwd varchar(100)  NOT NULL,
-  		nick_name varchar(200) NOT NULL comment '用户名称',
-    	
-  		sex int(2) unsigned default 0 comment '性别0男人，1女人',
-  		auth int(2) unsigned default 0 comment '权限字段',
-    	PRIMARY KEY (_id)
-		) COMMENT = '用户表';
-	"""
-	create_account_sql="""
-		CREATE TABLE account (
-  		_id int(10) unsigned NOT NULL AUTO_INCREMENT,
-  		id int(10) unsigned unique  NOT NULL,
-  		uid int(10) unsigned NOT NULL,
-  		type int(2) unsigned default 0 comment '0现金，1银行卡，2信用卡，3.虚拟账号，4.投资账户',
-  		name varchar(200)   NOT NULL comment '账户名称',
-  		balance  float default 0 comment '账户金额',
-		remark  text comment '备注',
-  		create_time long NOT NULL,
-  		modify_time long NOT NULL,
-    	PRIMARY KEY (_id)
-		) COMMENT = '账户表';
-	"""
-	create_category_sql="""
-		CREATE TABLE category (
-  		_id int(10) unsigned NOT NULL AUTO_INCREMENT,
-  		id int(10) unsigned unique  NOT NULL,
-  		name varchar(200)   NOT NULL comment '目录名称',
-  		type int(2)  unsigned NOT NULL  comment '目录类型 0收入，1支出，2转账，3，借出，4.借入，5，还出，6，还入',
-    	PRIMARY KEY (_id)
-		) COMMENT = '目录表';
-	"""
-	create_category_sub_sql="""
-		CREATE TABLE category_sub(
-  		_id int(10) unsigned NOT NULL AUTO_INCREMENT,
-  		id int(10) unsigned unique  NOT NULL,
-  		pid int(10) unsigned NOT NULL,
-  		name varchar(200)   NOT NULL comment '目录名称',
-    	PRIMARY KEY (_id)
-		) COMMENT = '目录子表';
-	"""
-	create_record_sql="""
-		CREATE TABLE record (
-	  	_id int(10) unsigned NOT NULL AUTO_INCREMENT,
-	  	id int(10) unsigned unique NOT NULL,
-	   	cate_id int(10) unsigned NOT NULL comment '类型id',
-	   	uid int(10) unsigned  NOT NULL comment '用户id',
-	   	account_id int(10) unsigned NOT NULL comment '账户id',
-	   	type int(2) unsigned  default 0 comment '关联类型 默认为0，0为手动输入，1为文案输入',
-	   	type_id int(10) unsigned   comment '数据记录id',
-	   	amount float unsigned default 0 comment '价格',
-	   	remark  text comment '备注',
-	   	occur_time long NOT NULL,
-	   	status int(2) unsigned default 0 comment '状态0， 1不可用',
-	   	original_id  int(10) unsigned NOT NULL,
-	  	PRIMARY KEY (_id)
-		) COMMENT = '消费记录表';
-	"""
-
-	batch_sql(cursor,create_test_sql,create_user_group_sql,create_user_sql,create_account_sql,create_category_sql,create_category_sub_sql,create_record_sql)
-	
-
-	
 
 def insert_data(cursor):
 	print('insert_data')
@@ -156,6 +64,19 @@ def insert_data(cursor):
 		VALUES ('3','2','test','123456','测试',0);
 	"""
 	batch_sql(cursor,insert_user_sql1,insert_user_sql2,insert_user_sql3)
+
+	###################account#####################
+
+	insert_account_sql1="""
+		INSERT INTO `account` (`id`,`uid`, `type`, `name`, `balance`,`remark`, `create_time`,`modify_time`) 
+		VALUES ('1','1','0','现金', '0', "",'1568131425','1568131425');
+    """
+    insert_account_sql2="""
+    	pppp
+    """
+    print("test:",insert_account_sql2)
+
+	# batch_sql(cursor,insert_account_sql1,insert_account_sql2)
 	
 def show_data(cursor):
 	print('show_data')
@@ -173,11 +94,39 @@ def show_data(cursor):
 	cursor.execute(show_user_group_sql)	
 	print("data:",cursor.fetchone())  
 	
-	###################user_group#####################
+	###################user#####################
 	show_user_sql="""
 		SELECT * FROM user;
 	"""
 	cursor.execute(show_user_sql)	
+	print("data:",cursor.fetchone())  
+	
+	###################account#####################
+	show_account_sql="""
+		SELECT * FROM account;
+	"""
+	cursor.execute(show_account_sql)	
+	print("data:",cursor.fetchone())  
+	
+	###################category#####################
+	show_category_sql="""
+		SELECT * FROM category;
+	"""
+	cursor.execute(show_category_sql)	
+	print("data:",cursor.fetchone())  
+	
+	###################category_sub#####################
+	show_category_sub_sql="""
+		SELECT * FROM category_sub;
+	"""
+	cursor.execute(show_category_sub_sql)	
+	print("data:",cursor.fetchone())  
+
+	###################record#####################
+	show_record_sql="""
+		SELECT * FROM record;
+	"""
+	cursor.execute(show_record_sql)	
 	print("data:",cursor.fetchone())  
 	
 	
@@ -198,8 +147,7 @@ print('init db')
 cursor=conn.cursor() #控制光标
 print('cursor:',cursor)
 init_db(cursor)
-clear_table(cursor)
-create_table(cursor)
+clear_data(cursor)
 insert_data(cursor)
 show_data(cursor)
 print('exit')
